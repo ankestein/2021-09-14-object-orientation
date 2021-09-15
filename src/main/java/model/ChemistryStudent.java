c885e9b7f5d811cf1fd7b446ec89ae6c09337451
Anke <a.stein@posteo.de>
2021-09-15 09:35:38 +0200

    Correct HistoryStudent and ChemistryStudent: remove public int id and public String name, which are defined in Student class

--- src/main/java/model/ChemistryStudent.java
+++ src/main/java/model/ChemistryStudent.java
@@ -1,8 +1,6 @@
 package model;
 
 public class ChemistryStudent extends Student{
-    private int id;
-    private String name;
 
     public int getNumberOfModules() {
         return 15;
--- src/main/java/model/HistoryStudent.java
+++ src/main/java/model/HistoryStudent.java
@@ -1,8 +1,6 @@
 package model;
 
 public class HistoryStudent extends Student{
-    private int id;
-    private String name;
 
     public int getNumberOfModules() {
         return 11;
